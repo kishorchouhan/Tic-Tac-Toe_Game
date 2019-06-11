2b73a86defe34ceede309f2be2d69cd0472c70b6
kishorchouhan <devkishorbwr@gmail.com>
2019-06-11 08:54:23 +0530

    Updates

---
+++ precache-manifest.29b63f537674579da5ebe7c38484176f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "188810afe96dd9cfacf8",
+    "url": "/Tic-Tac-Toe_Game/static/js/runtime~main.bcf6d965.js"
+  },
+  {
+    "revision": "3e7e8179471c7d7ef396",
+    "url": "/Tic-Tac-Toe_Game/static/js/main.9d0a1894.chunk.js"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/Tic-Tac-Toe_Game/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "3e7e8179471c7d7ef396",
+    "url": "/Tic-Tac-Toe_Game/static/css/main.1024ee44.chunk.css"
+  },
+  {
+    "revision": "f1f4e710a8daae2d32c29d5377fc8b01",
+    "url": "/Tic-Tac-Toe_Game/index.html"
+  }
+];
